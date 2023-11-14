deb9a10c353ba67cc316cae29f82039de38b0661
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2023-11-14 15:58:27 -0300

    criando função de inciar o jogo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,72 @@
 #include <locale.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
+#include <locale.h>
+#include "hangman.h"
 
 typedef struct {
 	char nome[50];
-	int vidas_restantes;;
+	int vidas_restantes;
 } JOGADOR;
 
+int iniciar_jogo(char* palavra, char* palavra_mostrada, char* letras_unicas, int quantidade_letras_unicas, JOGADOR * jogador_1){
+	
+	char letra_jogador;
+	int acerto = 0;
+	int acertos = 0;
+	int jogando = 1;
+	
+	while (jogando){
+		
+		system("cls");
+		printf("Vidas do jogador: %d\n", jogador_1->vidas_restantes);
+		printf("Acertos: %d\n", acertos);
+		printf("---------------\n");
+		printf("Palavra: %s\n", palavra_mostrada);
+		printf("---------------\n");
+		printf("Escolha uma letra: ");
+		scanf("%c", &letra_jogador);
+		fflush(stdin);
+		
+		for (int i = 0; i < quantidade_letras_unicas; i++){
+			if (letra_jogador == letras_unicas[i]){
+				acerto = 1;
+			}
+		}
+		
+		if (acerto){
+			acertos += 1;
+			for (int i = 0; i < strlen(palavra); i++){
+				if (letra_jogador == palavra[i]){
+					palavra_mostrada[i] = letra_jogador;
+				}
+			}
+		} else {
+			jogador_1->vidas_restantes -= 1;
+		}
+		
+		if (jogador_1->vidas_restantes == 0){
+			jogando = 0;
+		}
+		
+		if (acertos == strlen(letras_unicas)){
+			jogando = 0;
+		}
+		
+		acerto = 0;
+	}
+}
+
 int main(){
+	
+	// Define a linguagem local como português.
+	setlocale(0, "Portuguese");
+	
+	// Definindo o jogador da partida.
+	JOGADOR jogador_1;
+    strcpy(jogador_1.nome, "Nichollas");
+    jogador_1.vidas_restantes = 6;
 	
 	char palavra_aleatoria[] = {"banana"}; // Palavra aleatória selecionada.
 	int tamanho_palavra = strlen(palavra_aleatoria); // Tamanho da palavra aleatória selecionada.
@@ -49,11 +108,58 @@ int main(){
 	strncpy(letras_unicas, array_descart, quantidade_letras);
 	letras_unicas[quantidade_letras] = '\0';
 	
-	printf("---------------\n");
-	printf("Palavra aleatoria: %s\n", palavra_aleatoria);
-	printf("Letras unicas: %s\n", letras_unicas);
-	printf("Lista vazia: %s\n", lista_vazia);
-	printf("---------------\n");	
+	tela_carregamento();
+	
+	int logado = 1;
+	int opcao;
+	char nova_palavra[50];
+	
+	while (logado){
+		system("cls");
+		printf("----------------------------------\n");
+	    printf("|          Menu do Jogo          |\n");
+	    printf("----------------------------------\n");
+	    printf("|      1 - Iniciar o Jogo        |\n");
+	    printf("|      2 - Sair do Jogo          |\n");
+	    printf("|      3 - Adicionar Palavra     |\n");
+	    printf("----------------------------------\n");
+	    printf("Digite sua opção desejada: ");
+	    scanf("%d", &opcao);
+	    fflush(stdin);
+	    
+	    switch (opcao){
+	    	case 1:
+	    		iniciar_jogo(palavra_aleatoria, lista_vazia, letras_unicas, quantidade_letras, &jogador_1);
+	    		break;
+	    	case 2:
+	    		system("cls");
+				printf("----------------------------------\n");
+			    printf("|             Créditos           |\n");
+			    printf("----------------------------------\n");
+			    printf("|      1 - José Nichollas        |\n");
+			    printf("|      2 - João Victor           |\n");
+			    printf("|      3 - João Pedro            |\n");
+			    printf("|      4 - Rodrigo Cordeiro      |\n");
+			    printf("|      5 - Duan Oliveira         |\n");
+			    printf("----------------------------------\n");
+		    	logado = 0;
+	    		break;
+	    	case 3:
+		    	system("cls");
+				printf("----------------------------------\n");
+			    printf("|              Regras            |\n");
+			    printf("----------------------------------\n");
+			    printf("|   1 - Não deve ter sinal       |\n");
+			    printf("|   2 - Não deve ser muito longa |\n");
+			    printf("----------------------------------\n");
+			    printf("Digite a palavra desejada: ");
+		    	scanf("%50[^\n]s", nova_palavra);
+		    default:
+		    	break;
+		}
+	
+		system("pause");
+	}
 	
 	return 0;
 }
